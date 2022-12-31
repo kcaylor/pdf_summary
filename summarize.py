b3302db46fdeeed9efeced1e783e7f2d8b08b902
Kelly Caylor <caylor@ucsb.edu>
2022-12-31 15:43:18 -0800

    changed stop tag;

--- summarize.py
+++ summarize.py
@@ -16,10 +16,24 @@ openai.organization = os.getenv('ORGANIZATION_ID', None)
 openai.api_key = os.getenv('API_KEY', None)
 engine = "text-davinci-003"
 
-tldr_tag = "\n tl;dr:"
 #%%
 
 def get_paper_content(filename=None,use_pypdf=True):
+    """ Create pdf object from file
+
+    Parameters
+    ----------
+    filename : string, optional
+        filename for pdf, by default None
+    use_pypdf : bool, optional
+        choose which pdf library to use, by default True
+
+    Returns
+    -------
+    list
+        list of page objects, one per page in pdf
+
+    """    
     if filename and not use_pypdf:
         return pdfplumber.open(filename).pages
     elif filename and use_pypdf:
@@ -27,26 +41,39 @@ def get_paper_content(filename=None,use_pypdf=True):
         pdfReader = pypdf.PdfReader(pdfFileObj)
         return pdfReader.pages
         
-def summarize_page(page,tldr_tag=tldr_tag,
+def summarize_page(page,
+                   stop_tag='###---###',
                    type='college professor',
                    max_tokens=100):
+    """Summarizes pdf page using openai
+
+    Parameters
+    ----------
+    page : page object
+        single page of a pdf
+    stop_tag : string, optional
+        stop string for model, by default '###---###'
+    type : str, optional
+        reading level of summary output, by default 'college professor'
+    max_tokens : int, optional
+        maximum number of words in summary, by default 100
+
+    Returns
+    -------
+    string
+        Summary text from model
+    """    
     prompt = "Summarize this for a {type}:\n".format(type=type)
-    text = prompt + page.extract_text() + tldr_tag
+    text = prompt + page.extract_text() + stop_tag
     response = openai.Completion.create(engine="text-davinci-003",prompt=text,
             temperature=0.3,
             max_tokens=max_tokens,
             top_p=1,
             frequency_penalty=0,
             presence_penalty=0,
-            stop=[tldr_tag]
+            stop=[stop_tag]
         )
     return response['choices'][0]['text']
-
-#%%
-filename = 'test_file.pdf'
-paper_content = get_paper_content(filename=filename)
-#print(summarize_page(paper_content[3]))
-summary = {i:summarize_page(page) for i,page in enumerate(paper_content)}
 
 # %%
 
